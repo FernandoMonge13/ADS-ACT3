29ab674295a57ee4cecdaca49f84b69887950519
Fernando <fercoche369@gmail.com>
2020-11-27 23:26:09 -0600

    Final

---
+++ starwars-app/src/App.js
@@ -0,0 +1,18 @@
+// Fernando Monge Ramirez
+// Oscar Soto Varela 
+import React from 'react';
+import StarWarsPeople from './StarWarsPeople';
+
+export default class App extends React.Component {
+   
+    
+    render() {
+        return (
+            <div className="App">
+                <StarWarsPeople></StarWarsPeople>
+
+                
+            </div>
+        );
+    }
+}
